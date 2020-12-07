3d181ef8862e9c418eb0a2277f97744151a0b4eb
Gammaglobulino <mazzantia@hotmail.com>
2020-12-07 16:41:56 +0100

    added User profile

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use rand::Rng;
 use std::io;
+#[derive(Debug)]
+struct User {
+    name:String,
+    last_name:String
+}
+impl User{
+    fn new() -> User{
+        User{name:String::from(""),last_name:String::from("")}
+    }
+    
+}
 
 fn main() {
+
     println!("Guess a number:");
+    let mut usr=User::new();
+    println!("Please enter your name:");
+    io::stdin().read_line(&mut usr.name).expect("Error reading input");
+    println!("Please enter your last name:");
+    io::stdin().read_line(&mut usr.last_name).expect("Error reading input");
+    println!("Hello {:?}",usr);
+    
     let secret=rand::thread_rng().gen_range(1,100);
     
     loop{
